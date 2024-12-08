367439b6e724df1245fe065b171774852c4692b2
Maxim L. <38497831+StableBig@users.noreply.github.com>
2024-12-08 17:00:05 +0100

    Fork serialize post to serialize post optimized

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,23 @@ from blog.models import Comment, Post, Tag
 
 
 def serialize_post(post):
+    """Оригинальная функция, используем для post_detail и tag_filter"""
+    return {
+        'title': post.title,
+        'teaser_text': post.text[:200],
+        'author': post.author.username,
+        'comments_amount': post.comments_count if hasattr(post, 'comments_count') else 0,
+        'image_url': post.image.url if post.image else None,
+        'published_at': post.published_at,
+        'slug': post.slug,
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.first().title if post.tags.exists() else None,
+        'likes_amount': post.likes_count if hasattr(post, 'likes_count') else 0,
+    }
+
+
+def serialize_post_optimized(post):
+    """Оптимизированная функция для главной страницы (index)"""
     return {
         'title': post.title,
         'teaser_text': post.text[:200],
@@ -47,8 +64,8 @@ def index(request):
     )[:5]
 
     context = {
-        'most_popular_posts': [serialize_post(post) for post in most_popular_posts],
-        'page_posts': [serialize_post(post) for post in most_fresh_posts],
+        'most_popular_posts': [serialize_post_optimized(post) for post in most_popular_posts],
+        'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts],
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
